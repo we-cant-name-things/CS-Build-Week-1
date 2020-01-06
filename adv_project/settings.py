4e7faef97947051c4d76b539c44b6e3f063181d7
Sean Wu <seanwu20@gmail.com>
2020-01-06 15:50:19 -0500

    test2

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -86,7 +86,12 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 
