6340b0d2d9fd0143f9a4077c66815ce563391677
Sean Wu <seanwu20@gmail.com>
2020-01-08 12:25:34 -0500

    simplfied stuff

--- canada_adv/serializers.py
+++ canada_adv/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 from .models import Player, Place
 
 
-class PlayerSerializer(serializers.ModelSerializer):
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player
         fields = ['id', 'food', 'water', 'email', 'current_place']
+        lookup_field = 'email'
+
+
 
 
 class PlaceSerializer(serializers.ModelSerializer):
--- canada_adv/views.py
+++ canada_adv/views.py
@@ -11,21 +11,6 @@ class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
-    @action(methods=['get'], detail=False)
-    def player(self, request):
-        email = request.data['email']
-        query = self.get_queryset().get(email=email)
-        serializer = self.get_serializer_class()(query)
-        return Response(serializer.data)
-
-
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-
-    # @action(methods=['get'], detail=False)
-    # def player(self, request):
-    #     email = request.data['email']
-    #     query = self.get_queryset().get(email=email)
-    #     serializer = self.get_serializer_class()(query)
-    #     return Response(serializer.data)
