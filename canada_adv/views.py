f69d226e11e2d8ec761145b5f175da2a9b18ff6f
Sean Wu <seanwu20@gmail.com>
2020-01-08 22:22:19 -0500

    finished player endpoint

--- canada_adv/views.py
+++ canada_adv/views.py
@@ -1,5 +1,4 @@
 from .models import Player, map
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -13,12 +12,14 @@ class PlayerViewSet(viewsets.ModelViewSet):
 
 @api_view(["GET"])
 def player_info(request):
-    player_data = Player.objects.values().get(email=request.data['email'])
-    current_city = map.search_map(player_data['city'])
-    if current_city == -1:
-        return Response("Player City does not exit")
-    elif current_city != -1:
-        player_data['left'] = current_city.left.city
-        player_data['right'] = current_city.right.city
-
-    return Response(player_data)
+    try:
+        player_data = Player.objects.values().get(email=request.data['email'])
+        current_city = map.search_map(player_data['city'])
+        if current_city == -1:
+            return Response("Player City does not exit")
+        elif current_city != -1:
+            player_data['left'] = current_city.left.city
+            player_data['right'] = current_city.right.city
+        return Response(player_data)
+    except:
+        return Response("Invalid email")
